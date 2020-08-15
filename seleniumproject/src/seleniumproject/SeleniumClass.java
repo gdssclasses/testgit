c3d4569ec0ddad82294031c3ad1dfe1e43b4ed2e
User <User@DESKTOP-DHQI6EK>
2020-08-15 10:20:21 +0530

    this is test case 01

--- seleniumproject/src/seleniumproject/SeleniumClass.java
+++ seleniumproject/src/seleniumproject/SeleniumClass.java
@@ -1,34 +1,19 @@
-package seleniumproject;
+package seleniumproject; 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.chrome.ChromeDriver; 
 
 public class SeleniumClass {
 		
 	public static void main(String[] args) {
 	// declaration and instantiation of objects/variables
-	System.setProperty("webdriver.firefox.marionette","C:\\Users\\User\\Desktop\\Java\\geckodriver.exe");
-	WebDriver driver = new FirefoxDriver();
-	//comment the above 2 lines and uncomment below 2 lines to use Chrome
-	//System.setProperty("webdriver.chrome.driver","G:\\chromedriver.exe");
-	//WebDriver driver = new ChromeDriver();
-	String baseUrl = "http://www.foxnews.com/";
-	String expectedTitle = "Fox News - Breaking News Updates | Latest News Headlines | Photos & News Videos";
-	String actualTitle = "";
-	// launch Fire fox and direct it to the Base URL
-	driver.get(baseUrl);
-	// get the actual value of the title
-	actualTitle = driver.getTitle();
-	/*
-	* compare the actual title of the page with the expected one and print
-	* the result as "Passed" or "Failed"
-	*/
-	if (actualTitle.contentEquals(expectedTitle)){
-	System.out.println("Test Passed!");
-	} else {
-	System.out.println("Test Failed");
-	}
+	System.setProperty("webdriver.chrome.driver","C:\\Users\\User\\Desktop\\Java\\chromedriver.exe");
+	WebDriver driver = new ChromeDriver();;
+	driver.navigate().to("http://www.google.com/");
+	driver.findElement(By.name("q")).sendKeys("javatpoint tutorials");	
+	driver.findElement(By.name("q")).click();
 	//close Fire fox
-	driver.close();
+/*	driver.close(); */
 	}
-	}
+}
 
